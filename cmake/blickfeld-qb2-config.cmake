06e6d2ed745228a5cd60a1daee5e334d726b72db
Johannes Meßmer <johannes.messmer@blickfeld.com>
2024-12-19 13:19:39 +0000

    blickfeld/blickfeld-qb2!8 blickfeld/blickfeld-qb2#323 Fix cpp build with latest ubuntu distribution
    The custom gRPC find caused linking issues.
    The custom find is now only used for older distributions.
    
    Approved-by: Sven Martin <sven.martin@blickfeld.com>

--- cmake/blickfeld-qb2-config.cmake
+++ cmake/blickfeld-qb2-config.cmake
@@ -1,9 +1,6 @@
 include(CMakeFindDependencyMacro)
 
 if(NOT blickfeld-qb2_FOUND)
-    # Add custom finds
-    list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/third-party")
-
     # Protocol: Find Protobuf
     find_package(Protobuf 3.12.4)
     if(NOT Protobuf_FOUND)
@@ -12,7 +9,12 @@ if(NOT blickfeld-qb2_FOUND)
     endif()
 
     # Protocol: Find gRPC
-    find_dependency(gRPC)
+    find_dependency(gRPC QUIET)
+    if (NOT gRPC_FOUND)
+        # Fallback to custom gRPC find (due to Ubuntu 20.04)
+        list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/third-party")
+    endif()
+    find_dependency(gRPC REQUIRED)
 
     include("${CMAKE_CURRENT_LIST_DIR}/blickfeld-qb2.cmake")
 
